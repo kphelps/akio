8dc23a59592d36d3bb39347432053308e3fbe22d
Kyle Phelps <kylep91@gmail.com>
2017-10-11 19:28:00 -0400

    Add id helper in actors

--- akio/src/actor.rs
+++ akio/src/actor.rs
@@ -19,6 +19,10 @@ pub trait Actor {
 
     fn on_stop_impl(&mut self) {}
 
+    fn id(&self) -> Uuid {
+        context::with(|ctx| ctx.self_ref.id())
+    }
+
     fn with_children<F, R>(&self, f: F) -> R
     where
         F: FnOnce(&ActorChildren) -> R,
